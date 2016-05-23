675ddfe0a95731d2425a5863539c145f8f03cf8a
Densaugeo <you@example.com>
2016-05-23 13:11:05 -0400

    Add Arduino test script

---
+++ src/src.ino
@@ -0,0 +1,37 @@
+// This is a small test script for testing Skirnir on hardware
+
+#include "Skirnir180.hpp"
+
+unsigned char packet_decoded[180];
+unsigned long last_ping = 0;
+
+Skirnir a_skirnir = Skirnir(&Serial);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Check timer for overflow
+  if(millis() < last_ping) {
+    last_ping = 0;
+  }
+  
+  // Start heartbeat every 5s
+  if(millis() > last_ping + 5000) {
+    last_ping = millis();
+
+    a_skirnir.heartbeat();
+  }
+
+  // Returns true if a valid packet is found
+  switch(a_skirnir.receive_until_packet(packet_decoded)) {
+    // Echo packet back to PC
+    case 45:
+      a_skirnir.send(packet_decoded, 45);
+      break;
+    case 180:
+      a_skirnir.send(packet_decoded, 180);
+      break;
+  }
+}
