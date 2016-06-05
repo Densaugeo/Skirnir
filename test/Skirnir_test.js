b1dfcdbf8c06db541f3759af79add33cf0659899
Densaugeo <you@example.com>
2016-06-05 18:28:45 -0400

    Spelling

--- test/Skirnir_test.js
+++ test/Skirnir_test.js
@@ -12,25 +12,25 @@ function list_keys(object) {
 }
 
 skirnir.on('add', function(e) {
-  console.log('Added'.green + ' new serial device: ' + e.device.green + '. Currect devices are:');
+  console.log('Added'.green + ' new serial device: ' + e.device.green + '. Current devices are:');
   
   list_keys(skirnir.devices);
 });
 
 skirnir.on('remove', function(e) {
-  console.log('Removed'.red + ' serial device: ' + e.device.green + '. Currect devices are:');
+  console.log('Removed'.red + ' serial device: ' + e.device.green + '. Current devices are:');
   
   list_keys(skirnir.devices);
 });
 
 skirnir.on('connect', function(e) {
-  console.log('Connected'.green + ' device ' + e.device.green + '. Currect connections are:');
+  console.log('Connected'.green + ' device ' + e.device.green + '. Current connections are:');
   
   list_keys(skirnir.connections);
 });
 
 skirnir.on('disconnect', function(e) {
-  console.log('Disconnected'.red + ' device ' + e.device.green + '. Currect connections are:');
+  console.log('Disconnected'.red + ' device ' + e.device.green + '. Current connections are:');
   
   list_keys(skirnir.connections);
 });
